18e42a60d2e4285ebba0943719c452eeba8a5453
Mehul-Natu <natu.m@northeastern.edu>
2024-02-15 14:38:29 -0500

    Changes in vbariable.tf

--- variables.tf
+++ variables.tf
@@ -1,45 +1,35 @@
 variable "credentials_file_path" {
   type     = string
-  nullable = false
 }
 
 variable "project_id" {
   type     = string
-  nullable = false
 }
 
 variable "region" {
   type     = string
-  nullable = false
 }
 
 variable "zone" {
   type     = string
-  nullable = false
 }
 
 variable "vpc_name" {
   type     = string
-  nullable = false
 }
 
 variable "webapp_name" {
   type     = string
-  nullable = false
 }
 
 variable "db_Name" {
   type     = string
-  nullable = false
 }
-
 
 variable "webapp_subnet_cidr" {
   type     = string
-  nullable = false
 }
 
 variable "db_subnet_cidr" {
   type     = string
-  nullable = false
 }
